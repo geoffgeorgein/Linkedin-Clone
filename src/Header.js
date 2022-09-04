6e7bfb9af04ee7917a1fc671b7765c8702ff53b6
Geoff George <geoffgeorgethommana@gmail.com>
2022-09-04 10:17:48 +0530

    Header

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+
+
+function Header() {
+  return (
+    <div>Header
+    
+    <div className="Header_left">
+        <img></img>
+
+    </div>
+
+    <div className="Header_Search">
+        <SearchIcon></SearchIcon>
+        <input type="text">
+
+        </input>
+        
+    </div>
+
+    <div className="Header_right">
+
+            
+    </div>
+    </div>
+        )
+}
+
+export default Header
