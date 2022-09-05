20f7905542f979d833a26f3bc8b986c6b1d6242d
Geoff George <geoffgeorgethommana@gmail.com>
2022-09-05 22:39:35 +0530

    adds

--- src/Post.js
+++ src/Post.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Avatar from '@mui/material/Avatar';
 import './Post.css';
 
-function Post({name}) {
+function Post({name,description,message,photoUrl}) {
   return (
     <div className="post">
 
